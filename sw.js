aaf1a96eb9a29a797cf9b2d97508dc7589bbcd3c
dippie007-svg <dippie007@gmail.com>
2026-06-12 11:47:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sitenote-v31';
+const CACHE = 'sitenote-v36';
 const STATIC = ['./logo-dvm.jpg', './icon-192.png', './icon-512.png', './js/vendor/docx.umd.js'];
 
 self.addEventListener('install', e => {
